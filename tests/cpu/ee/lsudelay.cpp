ded9a694f7594250f1c35f45f5051fdbbc2d8d5a
Unknown W. Brackets <checkins@unknownbrackets.org>
2015-04-08 20:27:04 -0700

    Add a quick example for load delay slots.
    The ee doesn't have them, but the iop does.

---
+++ tests/cpu/ee/lsudelay.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <tamtypes.h>
+
+void test_lw_delay() {
+	const static u32 data = 0x13371337;
+	register u32 res = 0;
+	asm volatile (
+		".set noreorder\n"
+		"lui $t0, 0x1122\n"
+		"ori $t0, $t0, 0x3344\n"
+		"lw $t0, 0(%1)\n"
+		"or %0, $0, $t0\n"
+		"sync\n"
+		: "+&r"(res) : "r"((u32)&data) : "t0"
+	);
+	printf("lw: %08x\n", res);
+}
+
+int main(int argc, char *argv[]) {
+	printf("-- TEST BEGIN\n");
+
+	// Just to show that the ee (r5900) has no load delay slots.
+	test_lw_delay();
+
+	printf("-- TEST END\n");
+
+	return 0;
+}
