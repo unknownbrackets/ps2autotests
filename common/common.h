13b3021a24f7801a340a4974da3a79cfca95efb8
Unknown W. Brackets <checkins@unknownbrackets.org>
2015-04-18 21:58:02 -0700

    Adjust MSVC highlighting defines a bit.

--- common/common.h
+++ common/common.h
@@ -1,13 +1,14 @@
 #ifndef PS2AUTOTESTS_COMMON_H
 #define PS2AUTOTESTS_COMMON_H
 
-#include <stdio.h>
-#include <tamtypes.h>
-
 // Defines for MSVC highlighting.  Not intended to actually compile with msc.
 #ifdef _MSC_VER
-void printf(const char *s, ...);
+#define __STDC__
+#define _EE
 #define __attribute__(x)
 #endif
+
+#include <stdio.h>
+#include <tamtypes.h>
 
 #endif
