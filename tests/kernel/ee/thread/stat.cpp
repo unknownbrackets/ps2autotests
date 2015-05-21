f36b8e9977ba81960ba6872ad349e7847d3e52d1
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-05-21 01:05:17 -0400

    Added stat test with struct located in SPR.

--- tests/kernel/ee/thread/stat.cpp
+++ tests/kernel/ee/thread/stat.cpp
@@ -4,6 +4,7 @@
 
 #define INIT_PRIO 0x40
 #define STACK_SIZE 0x8000
+#define SPR_BEGIN 0x70000000
 
 char nullThreadStack[STACK_SIZE] __attribute__ ((aligned(16)));
 void nullThreadProc(u32) {
@@ -139,13 +140,21 @@ int main(int argc, char **argv) {
 
 	//Various corner cases
 	{
-		//Add thread stat from spr_ram
-
 		schedf("thread stat with invalid tid: ");
 		printThreadStatus(~0);
 
-		int result = ReferThreadStatus(0, NULL);
-		schedf("self thread stat with null struct: %02x\n", result);
+		{
+			int result = ReferThreadStatus(0, NULL);
+			schedf("self thread stat with null struct: %02x\n", result);
+		}
+
+		{
+			ee_thread_status_t* threadStat = (ee_thread_status_t*)SPR_BEGIN;
+			memset(threadStat, 0, sizeof(ee_thread_status_t));
+			int result = ReferThreadStatus(0, threadStat);
+			schedf("self thread stat with struct in spr -> result: %02x, status: %02x\n", 
+				result, threadStat->status);
+		}
 		
 		flushschedf();
 	}
