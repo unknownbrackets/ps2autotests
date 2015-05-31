3114ca32876627bb81e48e08e4b67510f735a2a3
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-05-30 23:28:17 -0400

    Added ee kernel ExitThread test.

---
+++ tests/kernel/ee/thread/exit.cpp
@@ -0,0 +1,78 @@
+#include <common-ee.h>
+#include <kernel.h>
+#include <string.h>
+
+#define STACK_SIZE 0x8000
+#define TEST_THREAD_PRIORITY 0x40
+
+char exitThreadStack[STACK_SIZE] __attribute__ ((aligned(16)));
+void exitThreadProc(u32) {
+	ExitThread();
+	SleepThread();
+}
+
+char exitDeleteThreadStack[STACK_SIZE] __attribute__ ((aligned(16)));
+void exitDeleteThreadProc(u32) {
+	ExitDeleteThread();
+	SleepThread();
+}
+
+int createTestThread(void *entry, int prio, void *stack, u32 stackSize) {
+	ee_thread_t threadParam;
+	memset(&threadParam, 0, sizeof(ee_thread_t));
+	threadParam.func             = entry;
+	threadParam.initial_priority = prio;
+	threadParam.stack            = stack;
+	threadParam.stack_size       = stackSize;
+	return CreateThread(&threadParam);
+}
+
+void printResult(u32 threadId, u32 result) {
+	if(threadId == result) {
+		schedf("returned thread id\n");
+	} else {
+		schedf("returned %d\n", result);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	printf("-- TEST BEGIN\n");
+
+	{
+		schedf("exit thread:\n");
+		
+		int threadId = createTestThread((void*)&exitThreadProc, TEST_THREAD_PRIORITY - 0x10, exitThreadStack, STACK_SIZE);
+		StartThread(threadId, NULL);
+		
+		s32 result = StartThread(threadId, NULL);
+		schedf("  start after thread exited -> ");
+		printResult(threadId, result);
+
+		result = DeleteThread(threadId);
+		schedf("  delete after thread exited -> ");
+		printResult(threadId, result);
+
+		flushschedf();
+	}
+
+	{
+		schedf("exit delete thread:\n");
+		
+		int threadId = createTestThread((void*)&exitDeleteThreadProc, TEST_THREAD_PRIORITY - 0x10, exitDeleteThreadStack, STACK_SIZE);
+		StartThread(threadId, NULL);
+
+		s32 result = StartThread(threadId, NULL);
+		schedf("  start after thread exited -> ");
+		printResult(threadId, result);
+
+		result = DeleteThread(threadId);
+		schedf("  delete after thread exited -> ");
+		printResult(threadId, result);
+
+		flushschedf();
+	}
+	
+	printf("-- TEST END\n");
+	
+	return 0;
+}
