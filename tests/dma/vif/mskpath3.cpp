80d855800cd158bc3ee8e0f9cb5ad7d18d6d8977
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2015-08-26 23:36:08 -0400

    Updated mskpath3 test to use DMA instead of FIFO.

--- tests/dma/vif/mskpath3.cpp
+++ tests/dma/vif/mskpath3.cpp
@@ -4,20 +4,13 @@
 #include <malloc.h>
 #include <string.h>
 #include <ee_regs.h>
+#include "../dmaregs.h"
+#include "../dmasend.h"
+#include "../dmatags.h"
 #include "emit_vifcode.h"
 #include "vifregs.h"
 
 #define EE_GIF_STAT_M3P (0x02)
-
-void send_simple_fifo_vif(volatile u128 *fifo, void *vifcode, int size) {
-	// Must be aligned to 16 bytes.
-	assert((size & 0xf) == 0 && (((u32)vifcode) & 0xf) == 0);
-
-	u128 *vif128 = (u128 *)vifcode;
-	for (int i = 0; i < size / 16; ++i) {
-		*fifo = vif128[i];
-	}
-}
 
 int main(int argc, char *argv[]) {
 	printf("-- TEST BEGIN\n");
@@ -27,17 +20,15 @@ int main(int argc, char *argv[]) {
 	{
 		VIF::Packet vifcode(16);
 		vifcode.MSKPATH3(true);
-
-		send_simple_fifo_vif(VIF::VIF1_FIFO, vifcode.Raw(), 16);
-
+		DMA::SendSimple(DMA::D1, vifcode.Raw(), 16);
+	
 		printf("mask value (after mskpath3(true))): %08X\n", *R_EE_GIF_STAT & EE_GIF_STAT_M3P);
 	}
 
 	{
 		VIF::Packet vifcode(16);
 		vifcode.MSKPATH3(false);
-
-		send_simple_fifo_vif(VIF::VIF1_FIFO, vifcode.Raw(), 16);
+		DMA::SendSimple(DMA::D1, vifcode.Raw(), 16);
 
 		printf("mask value (after mskpath3(false))): %08X\n", *R_EE_GIF_STAT & EE_GIF_STAT_M3P);
 	}
