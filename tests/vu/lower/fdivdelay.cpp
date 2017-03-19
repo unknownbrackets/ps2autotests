1f5891ffe15dd2b3696a344ec468ca69875e92ad
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2017-03-18 23:08:20 -0400

    Added pipeline stall test cases.

--- tests/vu/lower/fdivdelay.cpp
+++ tests/vu/lower/fdivdelay.cpp
@@ -185,6 +185,94 @@ public:
 			printf("  VF%02d: ", i); PrintRegister(static_cast<VU::Reg>(VF00 + i), true);
 		}
 	}
+	
+	void PerformStalled(const char *name, FdivFunction func, u32 stallDelay) {
+		using namespace VU;
+		
+		printf("%s stalled (%d):\n", name, stallDelay);
+		
+		Reset();
+		
+		//Set Q to 1.0f
+		Wr(DIV(FIELD_W, VF00, FIELD_W, VF00));
+		Wr(WAITQ());
+		
+		//Prepare registers
+		WrImm(4.0f);
+		WrImm(MULi(DEST_W, VF01, VF00), 16.0f);
+		Wr(MULi(DEST_W, VF02, VF00));
+		
+		//Op
+		Wr(func(FIELD_W, VF01));
+		Wr(MADD(DEST_XYZW, VF30, VF23, VF00));
+		for(u32 i = 0; i < stallDelay; i++) {
+			Wr(MULq(DEST_XYZW, VF00, VF00));
+		}
+		Wr(CLIP(VF30, VF30));
+		Wr(MULq(DEST_XYZW, VF01, VF00));
+		Wr(MULq(DEST_XYZW, VF02, VF00));
+		Wr(MULq(DEST_XYZW, VF03, VF00));
+		Wr(MULq(DEST_XYZW, VF04, VF00));
+		Wr(MULq(DEST_XYZW, VF05, VF00));
+		Wr(MULq(DEST_XYZW, VF06, VF00));
+		Wr(MULq(DEST_XYZW, VF07, VF00));
+		Wr(MULq(DEST_XYZW, VF08, VF00));
+		Wr(MULq(DEST_XYZW, VF09, VF00));
+		Wr(MULq(DEST_XYZW, VF10, VF00));
+		Wr(MULq(DEST_XYZW, VF11, VF00));
+		Wr(MULq(DEST_XYZW, VF12, VF00));
+		Wr(MULq(DEST_XYZW, VF13, VF00));
+		Wr(MULq(DEST_XYZW, VF14, VF00));
+		
+		Execute();
+		
+		for(u32 i = 1; i <= 14; i++) {
+			printf("  VF%02d: ", i); PrintRegister(static_cast<VU::Reg>(VF00 + i), true);
+		}
+	}
+	
+	void PerformStalledBranch(const char *name, FdivFunction func) {
+		using namespace VU;
+		
+		printf("%s stalled with branch:\n", name);
+		
+		Reset();
+		
+		//Set Q to 1.0f
+		Wr(DIV(FIELD_W, VF00, FIELD_W, VF00));
+		Wr(WAITQ());
+		
+		//Prepare registers
+		WrImm(4.0f);
+		WrImm(MULi(DEST_W, VF01, VF00), 16.0f);
+		Wr(MULi(DEST_W, VF02, VF00));
+		
+		//Op
+		Label label = L();
+		Wr(func(FIELD_W, VF01));
+		IBLTZ(MADD(DEST_XYZW, VF30, VF23, VF00), label, VI00);
+		Wr(CLIP(VF30, VF30));
+		Wr(MULq(DEST_XYZW, VF01, VF00));
+		Wr(MULq(DEST_XYZW, VF02, VF00));
+		Wr(MULq(DEST_XYZW, VF03, VF00));
+		Wr(MULq(DEST_XYZW, VF04, VF00));
+		Wr(MULq(DEST_XYZW, VF05, VF00));
+		Wr(MULq(DEST_XYZW, VF06, VF00));
+		Wr(MULq(DEST_XYZW, VF07, VF00));
+		Wr(MULq(DEST_XYZW, VF08, VF00));
+		Wr(MULq(DEST_XYZW, VF09, VF00));
+		Wr(MULq(DEST_XYZW, VF10, VF00));
+		Wr(MULq(DEST_XYZW, VF11, VF00));
+		Wr(MULq(DEST_XYZW, VF12, VF00));
+		Wr(MULq(DEST_XYZW, VF13, VF00));
+		Wr(MULq(DEST_XYZW, VF14, VF00));
+		
+		Execute();
+		
+		for(u32 i = 1; i <= 14; i++) {
+			printf("  VF%02d: ", i); PrintRegister(static_cast<VU::Reg>(VF00 + i), true);
+		}
+	}
 
 	void Perform(const char *name, FdivFunction func) {
 		using namespace VU;
@@ -193,6 +281,10 @@ public:
 		PerformSingleDelayed(name, func);
 		PerformMultipleDelayed1(name, func);
 		PerformMultipleDelayed2(name, func);
+		PerformStalled(name, func, 0);
+		PerformStalled(name, func, 1);
+		PerformStalled(name, func, 2);
+		PerformStalledBranch(name, func);
 	}
 };
 
