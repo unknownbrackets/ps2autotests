ec7b13a8628206523a50b5433c0f11b4b66c0d90
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2019-10-30 21:38:19 -0400

    Add some more tests.

--- tests/kernel/iop/libc/sprintf.c
+++ tests/kernel/iop/libc/sprintf.c
@@ -13,11 +13,19 @@ int _start(int argc, char **argv) {
 	EXECUTE_TEST(buffer, "%03d", 45);
 	EXECUTE_TEST(buffer, "%3d", 12);
 	EXECUTE_TEST(buffer, "%3.3d", 87);
+	EXECUTE_TEST(buffer, "%+6d", 948);
+	EXECUTE_TEST(buffer, "%+6d", -948);
+	EXECUTE_TEST(buffer, "%6d", -948);
+	EXECUTE_TEST(buffer, "%06d", -948);
+	EXECUTE_TEST(buffer, "%+06d", 948);
+	EXECUTE_TEST(buffer, "%u", -948);
 	EXECUTE_TEST(buffer, "%s%3.3d", "myfile", 0);
 	EXECUTE_TEST(buffer, "%x", 0xdeadbeef);
 	EXECUTE_TEST(buffer, "%X", 0xDEADBEEF);
 	EXECUTE_TEST(buffer, "%08x", 23);
 	EXECUTE_TEST(buffer, "%8x", 12);
+	EXECUTE_TEST(buffer, "%p", (void*)0x01234567);
+	EXECUTE_TEST(buffer, "%c", 'j');
 	
 	printf("-- TEST END\n");
 	return 0;
